5684b67e00c61d5e70a65e728060ab7f96c5c3fb
eduardasouzabandtec <eduarda.souza@bandtec.com.br>
2023-10-17 22:11:23 -0300

    feat: estrutura standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideRouter } from "@angular/router";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routing";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  
+bootstrapApplication(AppComponent, {providers: [provideRouter(routes), provideAnimations(), provideAnimations()]});
